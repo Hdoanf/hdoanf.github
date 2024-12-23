25543ab4f582f488bd14dbb131c4047a6e2965f3
Doan56 <bestbubuom@gmail.com>
2024-12-23 15:14:38 +0700

    lam lai

--- nguoidung.php
+++ nguoidung.php
@@ -2,25 +2,36 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "quanlytaisan";
+$dbname = "barcode";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Tìm kiếm
 $search = "";
-$loaitaisan = "";
-if (isset($_GET['search']) && isset($_GET['loaitaisan'])) {
-    $search = $_GET['search'];
-    $loaitaisan = $_GET['loaitaisan'];
-    $sql = "SELECT * FROM quan_ly_tai_san WHERE ten LIKE '%$search%' AND loai LIKE '%$loaitaisan%'";
-} else if (isset($_GET['loaitaisan'])) {
-    $loaitaisan = $_GET['loaitaisan'];
-    $sql = "SELECT * FROM quan_ly_tai_san WHERE loai LIKE '%$loaitaisan%'";
-} else if (isset($_GET['search'])) {
-    $search = $_GET['search'];
-    $sql = "SELECT * FROM quan_ly_tai_san WHERE ten LIKE '%$search%'";
-} else {
-    $sql = "SELECT * FROM quan_ly_tai_san";
-}
+$danhmuctaisan = "";
+$nhomtaisan = "";
+$tentaisan = "";
+$nam="";
+$trangthai="";
+
+
+
+
+// if (isset($_GET['search']) && isset($_GET['loaitaisan'])) {
+//     $search = $_GET['search'];
+//     $loaitaisan = $_GET['loaitaisan'];
+//     $sql = "SELECT * FROM quan_ly_tai_san WHERE ten LIKE '%$search%' AND loai LIKE '%$loaitaisan%'";
+// } else if (isset($_GET['loaitaisan'])) {
+//     $loaitaisan = $_GET['loaitaisan'];
+//     $sql = "SELECT * FROM quan_ly_tai_san WHERE loai LIKE '%$loaitaisan%'";
+// } else if (isset($_GET['search'])) {
+//     $search = $_GET['search'];
+//     $sql = "SELECT * FROM quan_ly_tai_san WHERE ten LIKE '%$search%'";
+// } else {
+//     $sql = "SELECT * FROM quan_ly_tai_san";
+// }
+
+
+$sql = "SELECT * FROM full_information";
 $kq = $conn->query($sql);
 ?>
 
@@ -48,46 +59,102 @@ $kq = $conn->query($sql);
                     <form method="GET" action="">
                         <input name="search" type="text" class="search-bar" placeholder="Tìm kiếm..."
                             value="<?php echo htmlspecialchars($search); ?>">
-                        <!-- <button type="submit">Tìm kiếm</button> -->
-                        <form method="GET" action="">
                             <select name="loaitaisan" class="search-bar">
-                                <option value="">Tất cả loại tài sản</option>
+                                <option value="">Tất cả nhóm tài sản</option>
                                 <?php
-                                $loaitaisan = $conn->query("SELECT DISTINCT loai FROM quan_ly_tai_san");
-                                if ($loaitaisan->num_rows > 0) {
-                                    while ($rowLoai = $loaitaisan->fetch_assoc()) {
-                                        echo "<option value='" . $rowLoai['loai'] . "'>"
-                                            . $rowLoai['loai'] . "</option>";
+                                $kqnhomtaisan = $conn->query("SELECT DISTINCT product_group FROM full_information");
+                                if ($kqnhomtaisan->num_rows > 0) {
+                                    while ($rownhom = $kqnhomtaisan->fetch_assoc()) {
+                                        $gr=$conn->query("SELECT `name_group` FROM `group_product` WHERE `id_group`='$rownhom[product_group]'");
+                                        $nhomten = $gr->fetch_assoc();
+                                        $tennhom = ($rownhom['product_group'] == $nhomtaisan )? "selected" : "";
+                                        echo "<option value='" . $rownhom['product_group'] . "' $tennhom >"
+                                         . $nhomten['name_group'] . "</option>";
                                     }
                                 }
                                 ?>
-                            </select><br>
+                            </select>
+                            <select name="phanloaitaisan" class="search-bar">
+                                <option value="">Tất cả phân loại tài sản</option>
+                                <?php
+                                $kqphanloai = $conn->query("SELECT DISTINCT product_category FROM full_information");
+                                if ($kqphanloai->num_rows > 0) {
+                                    while ($rowphanloai = $kqphanloai->fetch_assoc()) {
+                                        $pl=$conn->query("SELECT  `name_category` FROM `category_product` WHERE `id_category`= $rowphanloai[product_category]");
+                                        $tenloai = $pl->fetch_assoc();
+                                        $tenphanloai = ($rowphanloai['product_category'] == $danhmuctaisan)? "selected" : "";
+                                        echo "<option value='" . $rowphanloai['product_group'] . "' $tenphanloai >
+                                        ". $tenloai['name_category']."</option>";
+                
+                                    }
+                                }
+                                ?>
+                                
+                            </select>
+                            <select name="trangthai" class="search-bar">
+                                <option value="">Trạng thái</option>
+                                <?php
+                                $kqnhomtaisan = $conn->query("SELECT DISTINCT product_group FROM full_information");
+                                if ($kqnhomtaisan->num_rows > 0) {
+                                    while ($rownhom = $kqnhomtaisan->fetch_assoc()) {
+                                        $gr=$conn->query("SELECT `name_group` FROM `group_product` WHERE `id_group`='$rownhom[product_group]'");
+                                        $nhomten = $gr->fetch_assoc();
+                                        $tennhom = ($rownhom['product_group'] == $nhomtaisan )? "selected" : "";
+                                        echo "<option value='" . $rownhom['product_group'] . "' $tennhom >"
+                                         . $nhomten['name_group'] . "</option>";
+                                    }
+                                }
+                                ?>
+
+                            </select>
+                            <select name="nam" class="search-bar">
+                                <option value="">Năm</option>
+                                <?php
+                                $kqnhomtaisan = $conn->query("SELECT DISTINCT product_group FROM full_information");
+                                if ($kqnhomtaisan->num_rows > 0) {
+                                    while ($rownhom = $kqnhomtaisan->fetch_assoc()) {
+                                        $gr=$conn->query("SELECT `name_group` FROM `group_product` WHERE `id_group`='$rownhom[product_group]'");
+                                        $nhomten = $gr->fetch_assoc();
+                                        $tennhom = ($rownhom['product_group'] == $nhomtaisan )? "selected" : "";
+                                        echo "<option value='" . $rownhom['product_group'] . "' $tennhom >"
+                                         . $nhomten['name_group'] . "</option>";
+                                    }
+                                }
+                                ?>
+                                
+                            </select>
+                            <br>
                             <button type="submit" class="searchbtn">Tìm kiếm</button>
-                           
-                        </form>
-                         <br>
-                        </form>
+                        </form> <br>
                         <table class="table table-hover table-bordered">
                             <thead>
                                 <tr>
-                                    <th scope="col">MÃ</th>
-                                    <th scope="col">TÊN TÀI SẢN</th>
-                                    <th scope="col">TÌNH TRẠNG</th>
+                                    <th scope="col">TT</th>
+                                    <th scope="col">NHÓM TÀI SẢN</th>
                                     <th scope="col">LOẠI TÀI SẢN</th>
+                                    <th scope="col">TÊN TÀI SẢN</th>
+                                    <th scope="col">NĂM NHẬP</th>
+                                    <th scope="col">TRẠNG THÁI</th>
+                                    <th scope="col">THÔNG TIN TÀI SẢN</th>
                                     <th scope="col">TÁC VỤ</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <?php
+                                $tt = 1;
                                 if ($kq->num_rows > 0) {
                                     while ($row = $kq->fetch_assoc()) {
                                         echo "<tr>";
-                                        echo "<td>" . $row["ma"] . "</td>";
-                                        echo "<td>" . $row["ten"] . "</td>";
-                                        echo "<td>" . $row["tinh_trang"] . "</td>";
-                                        echo "<td>" . $row["loai"] . "</td>";
-                                        echo "<td><a href='chitiet.php?id=" . $row["ma"] . "'><button>Xem Thêm</button></a></td>";
+                                        echo "<td>" .$tt. "</td>";
+                                        echo "<td>" . $row["product_group"] . "</td>";
+                                        echo "<td>" . $row["product_category"] . "</td>";
+                                        echo "<td>" . $row["product_name"] . "</td>";
+                                        echo "<td>" . $row["year_import"] . "</td>";
+                                        echo "<td>" . $row["product_status"] . "</td>";
+                                        echo "<td>" . $row["product_information"] . "</td>";
+                                        echo "<td><a href='chitiet.php?id=" . $row["barcode"] . "'><button>Xem Thêm</button></a></td>";
                                         echo "</tr>";
+                                        $tt++;
                                     }
                                 } else {
                                     echo "<tr><td colspan='4'>Không có dữ liệu</td></tr>";
