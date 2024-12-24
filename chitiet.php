eaf6294c53546af0b65ccfe8aa8559db5c211f09
Doan56 <bestbubuom@gmail.com>
2024-12-24 22:54:30 +0700

    big update

--- chitiet.php
+++ chitiet.php
@@ -8,7 +8,7 @@ use Picqer\Barcode\BarcodeGeneratorHTML;
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "quanlytaisan";
+$dbname = "barcode";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 
@@ -20,12 +20,12 @@ if ($conn->connect_error) {
 $id = $_GET['id'];
 
 // lay du lieu
-$sql = "SELECT * FROM quan_ly_tai_san WHERE ma = '$id' ";
+$sql = "SELECT * FROM full_information WHERE barcode = '$id' ";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
-    $data = $row["ma"];
+    $data = $row["barcode"];
     $taoma = new BarcodeGeneratorHTML();
     $mavach = $taoma->getBarcode($data, $taoma::TYPE_CODE_128);
 } else {
@@ -51,35 +51,67 @@ if ($result->num_rows > 0) {
         <h1>Chi Tiết Tài Sản</h1>
         <table class="table table-bordered">
             <tr>
-                <th>Mã Tài Sản</th>
-                <td><?php echo $row["ma"]; ?></td>
+                <th>Nhóm Tài Sản </th>
+                <?php
+                $kqgroup = $conn->query("SELECT * FROM group_product WHERE id_group = " . $row["product_group"] . "");
+                if ($kqgroup->num_rows > 0) {
+                    $rowgroup = $kqgroup->fetch_assoc();
+                    $group = $rowgroup["name_group"];
+                }
+                echo "<td>" . $group . "</td>";
+                ?>
             </tr>
             <tr>
-                <th>Tên Tài Sản</th>
-                <td><?php echo $row["ten"]; ?></td>
+                <th>Loại Tài Sản</th>
+                <?php $kqcategory = $conn->query("SELECT * FROM category_product WHERE id_category = " . $row["product_category"] . "");
+                if ($kqcategory->num_rows > 0) {
+                    $rowcategory = $kqcategory->fetch_assoc();
+                    $category = $rowcategory["name_category"];
+                }
+                echo "<td>" . $category . "</td>"; ?>
             </tr>
             <tr>
                 <th>Tình Trạng</th>
-                <td><?php echo $row["tinh_trang"]; ?></td>
+                <?php $ten = strtoupper($row["product_name"]);
+                $kqname = $conn->query("SELECT * FROM `product` WHERE `id_product`='$ten'");
+                if ($kqname->num_rows > 0) {
+                    $rowname = $kqname->fetch_assoc();
+                    $name = $rowname["name_product"];
+                }
+                echo "<td>" . $name . "</td>";
+                ?>
             </tr>
             <tr>
                 <th>Ngày Cập Nhập</th>
-                <td><?php echo $row["ngay_cap_nhat"]; ?></td>
+                <?php $nam = '';
+                $kqnam = $conn->query("SELECT * FROM `year_import` WHERE `id_year`=" . $row["year_import"] . "");
+                if ($kqnam->num_rows) {
+                    $rownam = $kqnam->fetch_assoc();
+                    $nam = $rownam['year_import'];
+                }
+                echo "<td>" . $nam . "</td>"; ?>
             </tr>
+
             <tr>
                 <th>Mô tả</th>
-                <td><?php echo $row["mo_ta"];
+                <td> <?php echo $row["product_information"];
                 ?></td>
+
+            <tr>
+                <th>Trạng Thái</th>
+                <?php
+                $status = ($row["product_status"] == 'active') ? 'badge bg-success' : 'badge bg-danger';
+                echo "<td><span class='$status'>" . $row["product_status"] . "</span></td>"; ?>
+            </tr>
             <tr>
                 <th>Mã Vạch</th>
                 <td><?php echo $mavach; ?></td>
             </tr>
-            </tr>
-           
+
         </table>
         <a href="nguoidung.php" class="btn btn-primary">Quay lại</a>
-    </div> 
-    
+    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
--- nguoidung.php
+++ nguoidung.php
@@ -4,36 +4,17 @@ $username = "root";
 $password = "";
 $dbname = "barcode";
 $conn = new mysqli($servername, $username, $password, $dbname);
-
-// Tìm kiếm
 $search = "";
-$danhmuctaisan = "";
-$nhomtaisan = "";
-$tentaisan = "";
-$nam="";
-$trangthai="";
-
-
-
-
-// if (isset($_GET['search']) && isset($_GET['loaitaisan'])) {
-//     $search = $_GET['search'];
-//     $loaitaisan = $_GET['loaitaisan'];
-//     $sql = "SELECT * FROM quan_ly_tai_san WHERE ten LIKE '%$search%' AND loai LIKE '%$loaitaisan%'";
-// } else if (isset($_GET['loaitaisan'])) {
-//     $loaitaisan = $_GET['loaitaisan'];
-//     $sql = "SELECT * FROM quan_ly_tai_san WHERE loai LIKE '%$loaitaisan%'";
-// } else if (isset($_GET['search'])) {
-//     $search = $_GET['search'];
-//     $sql = "SELECT * FROM quan_ly_tai_san WHERE ten LIKE '%$search%'";
-// } else {
-//     $sql = "SELECT * FROM quan_ly_tai_san";
-// }
-
-
-$sql = "SELECT * FROM full_information";
+if(isset($_GET['search'])){
+    $search = $_GET['search'];
+    $sql = "SELECT * FROM `full_information` WHERE `product_name` LIKE '%$search%'";
+}else{
+    $sql = "SELECT * FROM full_information";
+}
 $kq = $conn->query($sql);
+
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -41,130 +22,186 @@ $kq = $conn->query($sql);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Phần Mềm Quản Lý Tài Sản</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+    <title>Quản lý tài sản </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 </head>
 
 <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="https://i.pinimg.com/736x/70/54/63/70546384b90a3b386bf16531c859d868.jpg" width="30"
+                    height="30" class="d-inline-block align-text-top me-2" alt="Logo">
+                Quản lý tài sản
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#"><i class="bi bi-house-door"></i> Dashboard</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="bi bi-person-circle"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
     <div class="container-fluid">
         <div class="row">
-            <nav class="col-md-3 col-lg-2 sidebar">
-                <h2>CHỨC NĂNG CỦA USER</h2>
-
+            <nav id="sidebar" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+                <div class="position-sticky pt-3">
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">
+                                <i class="bi bi-grid-1x2"></i> Tổng quát
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                Chức năng 1
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                Chức năng 2
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <i class="bi bi-gear"></i> Cài đặt
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </nav>
-            <main class="col-md-9 col-lg-10">
-                <div class="content">
-                    <h1>DANH SÁCH TÀI SẢN</h1>
-                    <form method="GET" action="">
-                        <input name="search" type="text" class="search-bar" placeholder="Tìm kiếm..."
-                            value="<?php echo htmlspecialchars($search); ?>">
-                            <select name="loaitaisan" class="search-bar">
-                                <option value="">Tất cả nhóm tài sản</option>
-                                <?php
-                                $kqnhomtaisan = $conn->query("SELECT DISTINCT product_group FROM full_information");
-                                if ($kqnhomtaisan->num_rows > 0) {
-                                    while ($rownhom = $kqnhomtaisan->fetch_assoc()) {
-                                        $gr=$conn->query("SELECT `name_group` FROM `group_product` WHERE `id_group`='$rownhom[product_group]'");
-                                        $nhomten = $gr->fetch_assoc();
-                                        $tennhom = ($rownhom['product_group'] == $nhomtaisan )? "selected" : "";
-                                        echo "<option value='" . $rownhom['product_group'] . "' $tennhom >"
-                                         . $nhomten['name_group'] . "</option>";
-                                    }
-                                }
-                                ?>
-                            </select>
-                            <select name="phanloaitaisan" class="search-bar">
-                                <option value="">Tất cả phân loại tài sản</option>
-                                <?php
-                                $kqphanloai = $conn->query("SELECT DISTINCT product_category FROM full_information");
-                                if ($kqphanloai->num_rows > 0) {
-                                    while ($rowphanloai = $kqphanloai->fetch_assoc()) {
-                                        $pl=$conn->query("SELECT  `name_category` FROM `category_product` WHERE `id_category`= $rowphanloai[product_category]");
-                                        $tenloai = $pl->fetch_assoc();
-                                        $tenphanloai = ($rowphanloai['product_category'] == $danhmuctaisan)? "selected" : "";
-                                        echo "<option value='" . $rowphanloai['product_group'] . "' $tenphanloai >
-                                        ". $tenloai['name_category']."</option>";
-                
-                                    }
-                                }
-                                ?>
-                                
-                            </select>
-                            <select name="trangthai" class="search-bar">
-                                <option value="">Trạng thái</option>
-                                <?php
-                                $kqnhomtaisan = $conn->query("SELECT DISTINCT product_group FROM full_information");
-                                if ($kqnhomtaisan->num_rows > 0) {
-                                    while ($rownhom = $kqnhomtaisan->fetch_assoc()) {
-                                        $gr=$conn->query("SELECT `name_group` FROM `group_product` WHERE `id_group`='$rownhom[product_group]'");
-                                        $nhomten = $gr->fetch_assoc();
-                                        $tennhom = ($rownhom['product_group'] == $nhomtaisan )? "selected" : "";
-                                        echo "<option value='" . $rownhom['product_group'] . "' $tennhom >"
-                                         . $nhomten['name_group'] . "</option>";
-                                    }
-                                }
-                                ?>
 
-                            </select>
-                            <select name="nam" class="search-bar">
-                                <option value="">Năm</option>
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <!-- chỗ tìm kiếm -->
+                <div class="card mt-4">
+                    <div class="card-body">
+                        <h2 class="card-title mb-4">Quản lý tài sản </h2>
+                        <form class="row g-3 mb-4" method="GET">
+                            <div class="col-md-4">
+                                <div class="input-group">
+                                    <span class="input-group-text"><i class="bi bi-search"></i></span>
+                                    <input name ="search" type="text" class="form-control " placeholder="Tìm kiếm..."
+                                    value="<?php echo htmlspecialchars($search)  ?>">
+                                    
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <select class="form-select">
+                                    <option selected>Asset Type</option>
+                                    <option>Hardware</option>
+                                    <option>Software</option>
+                                    <option>Network</option>
+                                </select>
+                            </div>
+                            <div class="col-md-3">
+                                <select class="form-select">
+                                    <option selected>Status</option>
+                                    <option>Active</option>
+                                    <option>Inactive</option>
+                                    <option>Maintenance</option>
+                                </select>
+                            </div>
+                            <div class="col-md-2">
+                                <button type="submit" class="btn btn-primary w-100">Search</button>
+                            </div>
+                        </form>
+                        <!-- bảng render từ database -->
+                        <div class="table-responsive">
+                            <table class="table table-hover text-center">
+                                <thead class="table-light">
+                                    <tr>
+                                    <th>TT</th>
+                                    <th>NHÓM TÀI SẢN</th>
+                                    <th>LOẠI TÀI SẢN</th>
+                                    <th>TÊN TÀI SẢN</th>
+                                    <th>NĂM NHẬP</th>
+                                    <th>TRẠNG THÁI</th>
+                                    <th>THÔNG TIN TÀI SẢN</th>
+                                    <th>TÁC VỤ</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
                                 <?php
-                                $kqnhomtaisan = $conn->query("SELECT DISTINCT product_group FROM full_information");
-                                if ($kqnhomtaisan->num_rows > 0) {
-                                    while ($rownhom = $kqnhomtaisan->fetch_assoc()) {
-                                        $gr=$conn->query("SELECT `name_group` FROM `group_product` WHERE `id_group`='$rownhom[product_group]'");
-                                        $nhomten = $gr->fetch_assoc();
-                                        $tennhom = ($rownhom['product_group'] == $nhomtaisan )? "selected" : "";
-                                        echo "<option value='" . $rownhom['product_group'] . "' $tennhom >"
-                                         . $nhomten['name_group'] . "</option>";
-                                    }
-                                }
-                                ?>
-                                
-                            </select>
-                            <br>
-                            <button type="submit" class="searchbtn">Tìm kiếm</button>
-                        </form> <br>
-                        <table class="table table-hover table-bordered">
-                            <thead>
-                                <tr>
-                                    <th scope="col">TT</th>
-                                    <th scope="col">NHÓM TÀI SẢN</th>
-                                    <th scope="col">LOẠI TÀI SẢN</th>
-                                    <th scope="col">TÊN TÀI SẢN</th>
-                                    <th scope="col">NĂM NHẬP</th>
-                                    <th scope="col">TRẠNG THÁI</th>
-                                    <th scope="col">THÔNG TIN TÀI SẢN</th>
-                                    <th scope="col">TÁC VỤ</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php
-                                $tt = 1;
+                                $tt=1;
+                                $status='';
+                                $group='';
+                                $category='';
+                                $name='';
+
                                 if ($kq->num_rows > 0) {
                                     while ($row = $kq->fetch_assoc()) {
                                         echo "<tr>";
-                                        echo "<td>" .$tt. "</td>";
-                                        echo "<td>" . $row["product_group"] . "</td>";
-                                        echo "<td>" . $row["product_category"] . "</td>";
-                                        echo "<td>" . $row["product_name"] . "</td>";
-                                        echo "<td>" . $row["year_import"] . "</td>";
-                                        echo "<td>" . $row["product_status"] . "</td>";
+                                        echo "<td>" . $tt . "</td>";
+                                        
+                                        //nhóm tài sản
+                                        $kqgroup = $conn->query("SELECT * FROM group_product WHERE id_group = " . $row["product_group"]. "");
+                                        if ($kqgroup->num_rows > 0) {
+                                            $rowgroup = $kqgroup->fetch_assoc();
+                                            $group = $rowgroup["name_group"];
+                                        }
+                                        echo "<td>" . $group. "</td>";
+
+                                        //loại tài sản
+                                        $kqcategory = $conn->query("SELECT * FROM category_product WHERE id_category = " . $row["product_category"]. "");
+                                        if ($kqcategory->num_rows > 0) {
+                                            $rowcategory = $kqcategory->fetch_assoc();
+                                            $category = $rowcategory["name_category"];
+                                        }
+                                        echo "<td>" . $category. "</td>";
+
+                                         //tên tài sản 
+                                         $ten=strtoupper($row["product_name"]);
+                                         $kqname = $conn->query("SELECT * FROM `product` WHERE `id_product`='$ten'");
+                                         if ($kqname->num_rows > 0) {
+                                             $rowname = $kqname->fetch_assoc();
+                                             $name = $rowname["name_product"];
+                                         }
+                                        echo "<td>" .$name. "</td>";
+                                        //năm
+                                         $nam='';
+                                         $kqnam=$conn->query("SELECT * FROM `year_import` WHERE `id_year`=". $row["year_import"] ."");
+                                         if($kqnam->num_rows){
+                                            $rownam=$kqnam->fetch_assoc();
+                                            $nam=$rownam['year_import'];
+                                         }
+                                        echo "<td>" .$nam. "</td>";
+                                        $status=($row["product_status"] == 'active') ? 'badge bg-success': 'badge bg-danger';
+                                        echo "<td><span class='$status'>" .$row["product_status"]. "</span></td>";
                                         echo "<td>" . $row["product_information"] . "</td>";
-                                        echo "<td><a href='chitiet.php?id=" . $row["barcode"] . "'><button>Xem Thêm</button></a></td>";
+                                        echo "<td><a href='chitiet.php?id=" . $row["barcode"] . "'><button  class='btn btn-sm btn-outline-primary' >Xem Thêm</button></a></td>";
                                         echo "</tr>";
                                         $tt++;
                                     }
-                                } else {
-                                    echo "<tr><td colspan='4'>Không có dữ liệu</td></tr>";
+                                }else {
+                                    echo "<tr><td colspan='7'>Không có dữ liệu</td></tr>";
                                 }
                                 ?>
-                            </tbody>
-                        </table>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </main>
         </div>
     </div>
+
+    <!-- <footer class="footer mt-auto py-3 bg-light">
+        <div class="container text-center">
+            <span class="text-muted">Phần mềm quản lý tài sản</span>
+        </div>
+    </footer> -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
 
 </html>
